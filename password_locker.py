f95398472b4cc652b7375ee4f6b4f325500eb0e7
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-06 08:32:47 +0300

    function to generate random  password

--- password_locker.py
+++ password_locker.py
@@ -23,3 +23,10 @@ def verify_user(first_name,password):
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
+
