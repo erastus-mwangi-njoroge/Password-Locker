914daf0e05ca9a9ef1e6163bf99a1f278d425f2d
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-06 08:41:29 +0300

    saving test-user function

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -8,3 +8,15 @@ class TestUser(unittest.TestCase):
 		
 		self.new_user = User('Erastus','Nj\'oro\'ge','uthiru10')
     
+	def test_init_(self):
+		
+		self.assertEqual(self.new_user.first_name,'Erastus')
+		self.assertEqual(self.new_user.last_name,'Nj\'oro\'ge')
+		self.assertEqual(self.new_user.password,'uthiru10')
+
+    def test_save_user(self):
+		
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
+    
+
