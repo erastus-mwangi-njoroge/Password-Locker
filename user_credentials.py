4678d55182d5135cc00e1a2c462e2136519c17f1
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-06 08:23:14 +0300

    function to generate password

--- user_credentials.py
+++ user_credentials.py
@@ -54,3 +54,10 @@ class Credential:
 		
 		Credential.credentials_list.append(self)
 	
+	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass
+
